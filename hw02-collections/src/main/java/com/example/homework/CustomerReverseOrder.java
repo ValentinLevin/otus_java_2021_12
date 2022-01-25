d685bb9249c3557fa35f7f891b3cd31c7de4afeb
Valentin <levin_valentin@mail.ru>
2022-01-25 15:55:29 +0600

    Замена типа переменной с LinkedList на Deque в классe com.example.CurtomerReverseOrder проекта hw02-collections

--- hw02-collections/src/main/java/com/example/homework/CustomerReverseOrder.java
+++ hw02-collections/src/main/java/com/example/homework/CustomerReverseOrder.java
@@ -1,19 +1,20 @@
 package com.example.homework;
 
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
-    private final LinkedList<Customer> list = new LinkedList<>();
+    private final Deque<Customer> stack = new LinkedList<>();
 
     //todo: 2. надо реализовать методы этого класса
     //надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
 
     public void add(Customer customer) {
-        this.list.push(customer);
+        this.stack.push(customer);
     }
 
     public Customer take() {
-        return this.list.pop();
+        return this.stack.pop();
     }
 }
